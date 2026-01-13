4934b412339cc62c9daa333eb1a12c46deb7226d
ZaidKhan26 <zzzaaaiiiddd044@gmail.com>
2026-01-13 15:29:57 +0530

    preparing for deployment

---
+++ hooks/use-fetch.js
@@ -0,0 +1,31 @@
+import { toast } from 'sonner';
+import { useState } from 'react';
+
+//const { useState, useCallback } = require('react');
+//const { set } = require('zod');
+
+const useFetch = (cb) => {
+    const [data, setData] = useState(undefined);
+    const [loading, setLoading] = useState(null);
+    const [error, setError] = useState(null);
+
+    const fn = async(...args) => {
+        setLoading(true);
+        setError(null);
+
+        try {
+            const response = await cb(...args);
+            setData(response);
+            setError(null);
+        } catch (error) {
+            setError(error);
+            toast.error(error.message);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return { data, loading, error, fn, setData };
+};
+
+export default useFetch;
